df068298068bc31e9b16802b54a16e9bd9d3f5bc
miraclemenikelechi <52224580+miraclemenikelechi@users.noreply.github.com>
2022-08-14 12:54:30 +0100

    a page about react facts

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,19 @@
 import React from "react";
+import background from "../img/react.png";
 
 export default function Main() {
     return (
         <section className="main-container">
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100k stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+            <div className="main-content">
+                <h1>Fun facts about React</h1>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
         </section>
     );
 }
